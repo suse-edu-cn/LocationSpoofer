d89beba206f1a9a7a1298e86ffe618adc2c9bebd
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-04-30 22:02:07 +0800

    修复无法移动选点的问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.suseoaa.locationspoofer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 124
-        versionName = "1.2.4"
+        versionCode = 125
+        versionName = "1.2.5"
 
         vectorDrawables {
             useSupportLibrary = true
